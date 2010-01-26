61cb5fe3c2cfed78662f2703f769657e64ecea1c
daniel.korzekwa <daniel.korzekwa@e34a1d74-e039-11de-9129-6395494df245>
2010-01-26 16:51:03 +0000

    Issue: 29 Implementing method to calculate delta between two heatMapModels.

--- betex-ecosystem-webconsole/src/main/java/dk/betex/ecosystem/webconsole/server/HeatMapModelFactory.java
+++ betex-ecosystem-webconsole/src/main/java/dk/betex/ecosystem/webconsole/server/HeatMapModelFactory.java
@@ -58,5 +58,42 @@ public class HeatMapModelFactory {
 		BioHeatMapModel heatMapModel = new BioHeatMapModel(columns );
 		return heatMapModel;
 	}
+	
+	/**Returns delta (difference) between model2 and model1. Delta = model2 - model1
+	 * 
+	 * Both models must have exactly the same number of columns and rows.
+	 * 
+	 * @param heatMapModel1
+	 * @param heatMapModel2
+	 * @return
+	 */
+	public static BioHeatMapModel delta(BioHeatMapModel heatMapModel1, BioHeatMapModel heatMapModel2) {
+		
+		if(heatMapModel2.getColumns().size()!= heatMapModel1.getColumns().size()) {
+			throw new IllegalArgumentException("Number of columns in both models is not the same.");
+		}
+	
+		List<HeatMapColumn> columns = new ArrayList<HeatMapColumn>();
+		for(int columnIndex=0;columnIndex<heatMapModel2.getColumns().size();columnIndex++) {
+			HeatMapColumn model2Column = heatMapModel2.getColumns().get(columnIndex);
+			HeatMapColumn model1Column = heatMapModel1.getColumns().get(columnIndex);
+			
+			if(model2Column.getValues().size()!=model1Column.getValues().size()) {
+				throw new IllegalArgumentException("Number of values in both columns for both models is not the same.");
+			}
+			
+			List<HeatMapValue> values = new ArrayList<HeatMapValue>();
+			for(int valueIndex=0;valueIndex<model2Column.getValues().size();valueIndex++) {
+				HeatMapValue model2Value = model2Column.getValues().get(valueIndex);
+				HeatMapValue model1Value = model1Column.getValues().get(valueIndex);
+					
+				values.add(new HeatMapValue(model2Value.getRowValue(),model2Value.getCellValue() - model1Value.getCellValue()));
+			}
+			
+			columns.add(new HeatMapColumn(model2Column.getLabel(),values));
+		}
+		BioHeatMapModel delta = new BioHeatMapModel(columns);
+		return delta;
+	}
 
 }
--- betex-ecosystem-webconsole/src/test/java/dk/betex/ecosystem/webconsole/server/HeatMapModelFactoryTest.java
+++ betex-ecosystem-webconsole/src/test/java/dk/betex/ecosystem/webconsole/server/HeatMapModelFactoryTest.java
@@ -14,11 +14,62 @@ import dk.betex.ecosystem.webconsole.client.components.bioheatmap.BioHeatMapMode
 
 public class HeatMapModelFactoryTest {
 
-	private BioHeatMapModel ds;
+	@Test
+	public void testCreateHeatMap() {
 
-	@Before
-	public void setUp() {
+		BioHeatMapModel ds1 = createModel1();
+		BioHeatMapModel heatMap = HeatMapModelFactory.createHeatMap(ds1, 0, 1);
 
+		assertEquals(ds1.getColumns().size(), heatMap.getColumns().size());
+
+		assertEquals(101, heatMap.getColumns().get(0).getValues().size());
+		assertEquals(765.56, heatMap.getColumns().get(0).getValues().get(45).getCellValue(), 0);
+		assertEquals(35.32, heatMap.getColumns().get(0).getValues().get(47).getCellValue(), 0);
+
+		assertEquals(101, heatMap.getColumns().get(1).getValues().size());
+		assertEquals(43.24, heatMap.getColumns().get(1).getValues().get(29).getCellValue(), 0);
+		assertEquals(65.12, heatMap.getColumns().get(1).getValues().get(27).getCellValue(), 0);
+	}
+
+	@Test
+	public void testDelta() {
+		BioHeatMapModel model1 = HeatMapModelFactory.createHeatMap(createModel1(), 0, 1);
+		BioHeatMapModel model2 = HeatMapModelFactory.createHeatMap(createModel2(), 0, 1);
+
+		BioHeatMapModel delta = HeatMapModelFactory.delta(model1, model2);
+
+		assertEquals(model2.getColumns().size(), delta.getColumns().size());
+
+		assertEquals(model2.getColumns().get(0).getValues().size(), delta.getColumns().get(0).getValues().size());
+		assertEquals(20, delta.getColumns().get(0).getValues().get(45).getCellValue(), 0);
+		assertEquals(2, delta.getColumns().get(0).getValues().get(47).getCellValue(), 0);
+		assertEquals(22.34, delta.getColumns().get(0).getValues().get(43).getCellValue(), 0);
+
+		assertEquals(model2.getColumns().get(1).getValues().size(), delta.getColumns().get(1).getValues().size());
+		assertEquals(5, delta.getColumns().get(1).getValues().get(29).getCellValue(), 0);
+		assertEquals(4.5, delta.getColumns().get(1).getValues().get(27).getCellValue(), 0);
+		assertEquals(17.43, delta.getColumns().get(1).getValues().get(26).getCellValue(), 0);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testDeltaNumberOfColumnsIsNotTheSame() {
+		BioHeatMapModel heatMapModel1 = HeatMapModelFactory.createHeatMap(createModel1(), 0, 1);
+		BioHeatMapModel heatMapModel2 = HeatMapModelFactory.createHeatMap(createModel2(), 0, 1);
+		heatMapModel1.getColumns().remove(0);
+		
+		HeatMapModelFactory.delta(heatMapModel1, heatMapModel2);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testDeltaNumberOfValuesIsNotTheSame() {
+		BioHeatMapModel heatMapModel1 = HeatMapModelFactory.createHeatMap(createModel1(), 0, 1);
+		BioHeatMapModel heatMapModel2 = HeatMapModelFactory.createHeatMap(createModel2(), 0, 1);
+		heatMapModel1.getColumns().get(0).getValues().remove(1);
+		
+		HeatMapModelFactory.delta(heatMapModel1, heatMapModel2);
+	}
+
+	private BioHeatMapModel createModel1() {
 		List<HeatMapColumn> columns = new ArrayList<HeatMapColumn>();
 
 		List<HeatMapValue> values1 = new ArrayList<HeatMapValue>();
@@ -30,24 +81,29 @@ public class HeatMapModelFactoryTest {
 		List<HeatMapValue> values2 = new ArrayList<HeatMapValue>();
 		values2.add(new HeatMapValue(3.4, 43.24));
 		values2.add(new HeatMapValue(3.6, 65.12));
-		HeatMapColumn heatMapColumn2 = new HeatMapColumn("ManUtd", values2);
+		HeatMapColumn heatMapColumn2 = new HeatMapColumn("Arsenal", values2);
 		columns.add(heatMapColumn2);
 
-		ds = new BioHeatMapModel(columns);
+		return new BioHeatMapModel(columns);
 	}
 
-	@Test
-	public void testCreateHeatMap() {
+	private BioHeatMapModel createModel2() {
+		List<HeatMapColumn> columns = new ArrayList<HeatMapColumn>();
 
-		BioHeatMapModel heatMap = HeatMapModelFactory.createHeatMap(ds, 0, 1);
+		List<HeatMapValue> values1 = new ArrayList<HeatMapValue>();
+		values1.add(new HeatMapValue(2.1, 37.32));
+		values1.add(new HeatMapValue(2.2, 785.56));
+		values1.add(new HeatMapValue(2.3, 22.34));
+		HeatMapColumn heatMapColumn1 = new HeatMapColumn("ManUtd", values1);
+		columns.add(heatMapColumn1);
 
-		assertEquals(ds.getColumns().size(), heatMap.getColumns().size());
-		assertEquals(101, heatMap.getColumns().get(0).getValues().size());
+		List<HeatMapValue> values2 = new ArrayList<HeatMapValue>();
+		values2.add(new HeatMapValue(3.4, 48.24));
+		values2.add(new HeatMapValue(3.6, 69.62));
+		values2.add(new HeatMapValue(3.8, 17.43));
+		HeatMapColumn heatMapColumn2 = new HeatMapColumn("Arsenal", values2);
+		columns.add(heatMapColumn2);
 
-		assertEquals(765.56, heatMap.getColumns().get(0).getValues().get(45).getCellValue(), 0);
-		assertEquals(35.32, heatMap.getColumns().get(0).getValues().get(47).getCellValue(), 0);
-
-		assertEquals(43.24, heatMap.getColumns().get(1).getValues().get(29).getCellValue(), 0);
-		assertEquals(65.12, heatMap.getColumns().get(1).getValues().get(27).getCellValue(), 0);
+		return new BioHeatMapModel(columns);
 	}
 }
