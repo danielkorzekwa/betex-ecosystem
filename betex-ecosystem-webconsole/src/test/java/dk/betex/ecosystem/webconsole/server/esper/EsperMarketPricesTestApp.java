f9b9063ebc991476a68fc86c47eabe8289b75898
daniel.korzekwa <daniel.korzekwa@e34a1d74-e039-11de-9129-6395494df245>
2010-01-29 15:29:56 +0000

    Issue: 37 Esper event processor - print delta of totalTradedVolume for 1 min time window - DONE

--- betex-ecosystem-webconsole/src/test/java/dk/betex/ecosystem/webconsole/server/esper/EsperMarketPricesTestApp.java
+++ betex-ecosystem-webconsole/src/test/java/dk/betex/ecosystem/webconsole/server/esper/EsperMarketPricesTestApp.java
@@ -2,6 +2,7 @@ package dk.betex.ecosystem.webconsole.server.esper;
 
 import java.util.Date;
 
+import org.apache.commons.math.util.MathUtils;
 import org.jcouchdb.db.Database;
 import org.jcouchdb.document.BaseDocument;
 import org.jcouchdb.document.ValueAndDocumentRow;
@@ -19,11 +20,8 @@ import dk.betex.ecosystem.marketdatacollector.dao.MarketDetailsDao;
 import dk.betex.ecosystem.marketdatacollector.dao.MarketDetailsDaoImpl;
 import dk.betex.ecosystem.marketdatacollector.dao.MarketPricesDao;
 import dk.betex.ecosystem.marketdatacollector.dao.MarketPricesDaoImpl;
-import dk.betex.ecosystem.marketdatacollector.dao.MarketTradedVolumeDao;
-import dk.betex.ecosystem.marketdatacollector.dao.MarketTradedVolumeDaoImpl;
 import dk.betex.ecosystem.marketdatacollector.model.MarketDetails;
 import dk.betex.ecosystem.marketdatacollector.model.MarketPrices;
-import dk.betex.ecosystem.marketdatacollector.model.MarketTradedVolume;
 
 /**
  * Process all marketTradedVolume events for market. Add event update listener that prints newly added event.
@@ -31,14 +29,14 @@ import dk.betex.ecosystem.marketdatacollector.model.MarketTradedVolume;
  * @author korzekwad
  * 
  */
-public class EsperTestApp {
+public class EsperMarketPricesTestApp {
 
 	private EPServiceProvider epService;
 	
-	private MarketTradedVolumeDao marketTradedVolueDao;
 	private MarketPricesDao marketPricesDao;
 	private MarketDetailsDao marketDetailsDao;
 	
+	private String dbUrl = "10.2.2.72";
     private long marketId=101081282l;
     private long twentyMinBeforeMarketTime;
 
@@ -47,49 +45,22 @@ public class EsperTestApp {
 		epService = EPServiceProviderManager.getDefaultProvider();
 		epService.initialize();
 		
-		EPStatement statement = epService.getEPAdministrator().createEPL("select 'marketTradedVolume' as eventType, timestamp,totalTradedVolume from dk.betex.ecosystem.marketdatacollector.model.MarketTradedVolume.win:ext_timed(timestamp,10 sec)");
-		statement.addListener(new EventLister());
-		EPStatement statement2 = epService.getEPAdministrator().createEPL("select 'marketPrices' as eventType,timestamp,totalTradedVolume from dk.betex.ecosystem.marketdatacollector.model.MarketPrices.win:ext_timed(timestamp,10 sec)");
-		statement2.addListener(new EventLister());
-
 		/** Init DAOs */
-		marketTradedVolueDao = new MarketTradedVolumeDaoImpl(new Database("10.2.2.72", "market_traded_volume"));
-		marketPricesDao = new MarketPricesDaoImpl(new Database("10.2.2.72", "market_prices"));
-		marketDetailsDao = new MarketDetailsDaoImpl(new Database("10.2.2.72", "market_details"));
+		marketPricesDao = new MarketPricesDaoImpl(new Database(dbUrl, "market_prices"));
+		marketDetailsDao = new MarketDetailsDaoImpl(new Database(dbUrl, "market_details"));
 		
 		/**Get market time.*/
 		MarketDetails marketDetails = marketDetailsDao.getMarketDetails(marketId);
 		twentyMinBeforeMarketTime = marketDetails.getMarketTime() - (1000*60*20);
 		
 	}
-
-	@Test
-	public void testProcessMarketTradedVolume() {
-		long now = System.currentTimeMillis();
-    		
-		/** Get first 200 of records. */
-		ViewAndDocumentsResult<BaseDocument, MarketTradedVolume> marketTradedVolumeList = marketTradedVolueDao
-				.getMarketTradedVolume(marketId,twentyMinBeforeMarketTime, Long.MAX_VALUE, 200);
-		for (ValueAndDocumentRow<BaseDocument, MarketTradedVolume> row : marketTradedVolumeList.getRows())
-			epService.getEPRuntime().sendEvent(row.getDocument());
-
-		/** Page through the rest of records. */
-		while (marketTradedVolumeList.getRows().size() > 0) {
-			marketTradedVolumeList = marketTradedVolueDao.getMarketTradedVolume(101081282l, marketTradedVolumeList
-					.getRows().get(marketTradedVolumeList.getRows().size() - 1).getDocument().getTimestamp() + 1,
-					Long.MAX_VALUE, 200);
-
-			for (ValueAndDocumentRow<BaseDocument, MarketTradedVolume> row : marketTradedVolumeList.getRows())
-				epService.getEPRuntime().sendEvent(row.getDocument());
-		}
-
-		System.out.println("Processing market traded volume: " + (System.currentTimeMillis() - now));
-
-	}
 	
 	@Test
 	public void testProcessMarketPrices() {
 		long now = System.currentTimeMillis();
+		
+		EPStatement statement2 = epService.getEPAdministrator().createEPL("select timestamp,totalTradedVolume,max(totalTradedVolume) - min(totalTradedVolume) as delta from dk.betex.ecosystem.marketdatacollector.model.MarketPrices.win:ext_timed(timestamp,10 sec)");
+		statement2.addListener(new EventLister());
        
 		/** Get first 200 of records. */
 		ViewAndDocumentsResult<BaseDocument, MarketPrices> marketPricesList = marketPricesDao.get(marketId,twentyMinBeforeMarketTime, Long.MAX_VALUE, 200);
@@ -98,7 +69,7 @@ public class EsperTestApp {
 
 		/** Page through the rest of records. */
 		while (marketPricesList.getRows().size() > 0) {
-			marketPricesList = marketPricesDao.get(101081282l, marketPricesList
+			marketPricesList = marketPricesDao.get(marketId, marketPricesList
 					.getRows().get(marketPricesList.getRows().size() - 1).getDocument().getTimestamp() + 1,
 					Long.MAX_VALUE, 200);
 
@@ -114,10 +85,10 @@ public class EsperTestApp {
 
 		@Override
 		public void update(EventBean[] newEvents, EventBean[] oldEvents) {
-			String eventType = (String)newEvents[0].get("eventType");
 			Date timestamp = new Date((Long) newEvents[0].get("timestamp"));
 			double totalTradedVolume = (Double) newEvents[0].get("totalTradedVolume");
-			System.out.println(eventType + ":" + newEvents.length + ":" + timestamp + ":" + totalTradedVolume);
+			double delta = (Double)newEvents[0].get("delta");
+			System.out.println(newEvents.length + ":" + timestamp + ":" + MathUtils.round(totalTradedVolume,2) + ":" + MathUtils.round(delta,2));
 		}
 
 	}
---
+++ betex-ecosystem-webconsole/src/test/java/dk/betex/ecosystem/webconsole/server/esper/EsperMarketTradedVolumeTestApp.java
@@ -0,0 +1,101 @@
+package dk.betex.ecosystem.webconsole.server.esper;
+
+import java.util.Date;
+
+import org.apache.commons.math.util.MathUtils;
+import org.jcouchdb.db.Database;
+import org.jcouchdb.document.BaseDocument;
+import org.jcouchdb.document.ValueAndDocumentRow;
+import org.jcouchdb.document.ViewAndDocumentsResult;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPServiceProviderManager;
+import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.UpdateListener;
+
+import dk.betex.ecosystem.marketdatacollector.dao.MarketDetailsDao;
+import dk.betex.ecosystem.marketdatacollector.dao.MarketDetailsDaoImpl;
+import dk.betex.ecosystem.marketdatacollector.dao.MarketPricesDao;
+import dk.betex.ecosystem.marketdatacollector.dao.MarketPricesDaoImpl;
+import dk.betex.ecosystem.marketdatacollector.dao.MarketTradedVolumeDao;
+import dk.betex.ecosystem.marketdatacollector.dao.MarketTradedVolumeDaoImpl;
+import dk.betex.ecosystem.marketdatacollector.model.MarketDetails;
+import dk.betex.ecosystem.marketdatacollector.model.MarketPrices;
+import dk.betex.ecosystem.marketdatacollector.model.MarketTradedVolume;
+
+/**
+ * Process all marketTradedVolume events for market. Add event update listener that prints newly added event.
+ * 
+ * @author korzekwad
+ * 
+ */
+public class EsperMarketTradedVolumeTestApp {
+
+	private EPServiceProvider epService;
+	
+	private MarketTradedVolumeDao marketTradedVolueDao;
+	private MarketDetailsDao marketDetailsDao;
+	
+	private String dbUrl = "10.2.2.72";
+    private long marketId=101081282l;
+    private long twentyMinBeforeMarketTime;
+
+	@Before
+	public void before() {
+		epService = EPServiceProviderManager.getDefaultProvider();
+		epService.initialize();
+		
+		/** Init DAOs */
+		marketTradedVolueDao = new MarketTradedVolumeDaoImpl(new Database(dbUrl, "market_traded_volume"));
+		marketDetailsDao = new MarketDetailsDaoImpl(new Database(dbUrl, "market_details"));
+		
+		/**Get market time.*/
+		MarketDetails marketDetails = marketDetailsDao.getMarketDetails(marketId);
+		twentyMinBeforeMarketTime = marketDetails.getMarketTime() - (1000*60*20);
+		
+	}
+
+	@Test
+	public void testProcessMarketTradedVolume() {
+		long now = System.currentTimeMillis();
+		
+		EPStatement statement = epService.getEPAdministrator().createEPL("select timestamp,totalTradedVolume,max(totalTradedVolume) - min(totalTradedVolume) as delta from dk.betex.ecosystem.marketdatacollector.model.MarketTradedVolume.win:ext_timed(timestamp,10 sec)");
+		statement.addListener(new EventLister());
+    		
+		/** Get first 200 of records. */
+		ViewAndDocumentsResult<BaseDocument, MarketTradedVolume> marketTradedVolumeList = marketTradedVolueDao
+				.getMarketTradedVolume(marketId,twentyMinBeforeMarketTime, Long.MAX_VALUE, 200);
+		for (ValueAndDocumentRow<BaseDocument, MarketTradedVolume> row : marketTradedVolumeList.getRows())
+			epService.getEPRuntime().sendEvent(row.getDocument());
+
+		/** Page through the rest of records. */
+		while (marketTradedVolumeList.getRows().size() > 0) {
+			marketTradedVolumeList = marketTradedVolueDao.getMarketTradedVolume(marketId, marketTradedVolumeList
+					.getRows().get(marketTradedVolumeList.getRows().size() - 1).getDocument().getTimestamp() + 1,
+					Long.MAX_VALUE, 200);
+
+			for (ValueAndDocumentRow<BaseDocument, MarketTradedVolume> row : marketTradedVolumeList.getRows())
+				epService.getEPRuntime().sendEvent(row.getDocument());
+		}
+
+		System.out.println("Processing market traded volume: " + (System.currentTimeMillis() - now));
+
+	}
+	
+	private class EventLister implements UpdateListener {
+
+		@Override
+		public void update(EventBean[] newEvents, EventBean[] oldEvents) {
+			
+			Date timestamp = new Date((Long) newEvents[0].get("timestamp"));
+			double totalTradedVolume = (Double) newEvents[0].get("totalTradedVolume");
+			double delta = (Double)newEvents[0].get("delta");
+			System.out.println(newEvents.length + ":" + timestamp + ":" + MathUtils.round(totalTradedVolume,2) + ":" + MathUtils.round(delta,2));
+	
+		}
+
+	}
+}
